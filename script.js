2eb6e313510b0a37530d5b154fd907db549387b9
sasi_p <sweetpea_th@hotmail.com>
2020-08-30 16:30:04 -0400

    Will come back to this later. Not working yet

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const buttons = document.getElementsByClassName('colourButton');
-
-function setButtonColour(button, red, green, blue) {
-    button.setAttribute('style', 'background-color: rgb('+ red +','+ green +','+ blue +');'
-    );
-}
-
-setButtonColour(buttons[0], 0, 0, 255);
-
 function makeColourValue() {
-    return Math.round(Math.random()*255);
-}
-
-const red = makeColourValue();
-const green = makeColourValue();
-const blue = makeColouoralue();
-
-setButtonColour(buttons[0], red, green, blue);
+    return Math.round(Math.random() * 255);
+  }
+  
+  function setButtonColour(button, red, green, blue) {
+    button.setAttribute('style',
+      'background-color: rgb(' + red + ',' + green + ',' + blue + ');'
+    );
+  }
+  
+  
+  
+  var buttons = document.getElementsByClassName('colourButton');
+  
+  var heading = document.getElementById('colourValue');
+  
+  var answerMessage = document.getElementById('answer');
+  
+  var answerButton = Math.round(Math.random() * (buttons.length - 1));
+  
+  
+  for (var i = 0; i < buttons.length; i++) {
+  
+  var red = makeColourValue();
+  var green = makeColourValue();
+  var blue = makeColourValue();
+  
+  setButtonColour(buttons[i], red, green, blue);
+  
+  if (i === answerButton) {
+      heading.innerHTML =`(${red}, ${green}, ${blue})`;
+  }
+  
+  buttons[i].addEventListener('click', function(){
+      if (this === buttons[answerButton]) {
+          answerMessage.innerHTML = "Correct!";
+      } else {
+          answerMessage.innerHTML = "Wrong answer! Guess again!";
+      }
+  });
+  
+  }
